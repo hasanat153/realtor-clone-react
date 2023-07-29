730f14bcc94b4bd89426ce4f2518097c99c76df0
unknown <hasanat153@gmail.com>
2023-07-29 21:52:25 +0600

    add Map leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -176,14 +176,14 @@ export default function Listing() {
             style={{ height: "100%", width: "100%" }}
           >
             <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker
               position={[listing.geolocation.lat, listing.geolocation.lng]}
             >
               <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+               {listing.address}
               </Popup>
             </Marker>
           </MapContainer>
